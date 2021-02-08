bea588a585ad800cfb788ffb01c4ccee46a3f9f9
jerome <jerome.henry3@gmail.com>
2021-02-08 22:36:51 +0100

    tried rxjs/ajax

--- src/components/SpeciesListButton/SpeciesListButton.tsx
+++ src/components/SpeciesListButton/SpeciesListButton.tsx
@@ -1,34 +1,41 @@
 import './SpeciesListButton.scss';
 import { useEffect, useState } from 'react';
-
+import { AjaxError, ajaxGetJSON } from 'rxjs/internal/observable/dom/AjaxObservable';
 
 
 const SpeciesListButton: React.FC<{}> = () => {
-    const [labels, setLabels] = useState([])
+    const [labels, setLabels] = useState<string[]>([])
     const [showLabels, setShowLabels] = useState(false);
+    const labelsObservable$ = ajaxGetJSON<string[]>(
+        'https://jeromehenry.fr/lichens-api/get_labels',
+        {  cache: "no-store" }
+    );
 
     useEffect(() => {
-        fetch('https://jeromehenry.fr/lichens-api/get_labels', {
-            // fetch('http://127.0.0.1:5000/get_labels', {
-              method: "GET",
-              headers: {
-                "Content-Type": "application/json"
-              },
-              cache: "no-store"
-            })
-            .then(response => response.json())
-            .then(labels => { setLabels(labels); })
-    })
+        labelsObservable$.subscribe(
+            (value: string[]) => {
+                setLabels(value);
+            },
+            (error: AjaxError) => {
+                console.error(error);
+            }
+        );
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    const copySpeciesName: React.MouseEventHandler = (event: React.MouseEvent) => { console.log(event); };
+
     return (
         <>
             <div className={"species-list " + (showLabels ? 'show': '')}>
-                <div className="title"> Expèces connues par le système : { labels.length } <br />
+                <div className="title">Expèces connues par le système : { labels.length } <br />
                 (en développement)</div>
                 {
                     labels.length && labels.map(label => (
                         <div
                             className="species-label"
                             key={label}
+                            onClick={(event) => copySpeciesName(event)}
                         >{ label }</div>
                     ))
                 }
